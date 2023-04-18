a11ccc7a2754b47c353b21ce2dced49e55e3e99a
Edvin Frosterud <edvinfro@kth.se>
2023-04-18 11:25:17 +0200

    Trying to add button

--- app/src/main/java/com/group7/meetr/MainActivity.java
+++ app/src/main/java/com/group7/meetr/MainActivity.java
@@ -3,6 +3,9 @@ package com.group7.meetr;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main); // set till en xml view
+
+        Button button = (Button) findViewById(R.id.button_test_to_login);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Log.d("BUTTONS", "User tapped the Supabutton");
+                setContentView(R.layout.login_page);
+            }
+        });
     }
 }
