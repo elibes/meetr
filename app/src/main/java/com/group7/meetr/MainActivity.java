b3124125a866886ebd589ee3b8f03d8c4b26c378
Edvin Frosterud <edvinfro@kth.se>
2023-04-18 11:54:16 +0200

    XML file for login. First edition login code. Should be moved to seperate model for different.
    We should get one xml file per state to differentiate.

--- app/src/main/java/com/group7/meetr/MainActivity.java
+++ app/src/main/java/com/group7/meetr/MainActivity.java
@@ -6,19 +6,27 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main); // set till en xml view
 
-        Button button = (Button) findViewById(R.id.button_test_to_login);
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login_page); // set till en xml view
+        Button button = (Button) findViewById(R.id.btnConfirmEmail);
         button.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 Log.d("BUTTONS", "User tapped the Supabutton");
-                setContentView(R.layout.login_page);
+                TextView txt = findViewById(R.id.txtViewHint);
+                //txt.setAllCaps(true);
+                TextView txt2 = findViewById(R.id.editTextTextEmailAddress);
+
+                Log.d("DEBUG_EMAIL_OUTPUT", txt2.getText().toString());
+
+                // för att få output till nästa steg ta txt2.gettext
+
             }
         });
     }
