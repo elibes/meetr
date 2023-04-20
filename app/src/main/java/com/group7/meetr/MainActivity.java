e2016d2554633c0510295534d77b06d6e323e52c
Edvin Frosterud <edvinfro@kth.se>
2023-04-20 15:47:41 +0200

    Försöker fixa gitignore.

--- app/src/main/java/com/group7/meetr/MainActivity.java
+++ app/src/main/java/com/group7/meetr/MainActivity.java
@@ -11,7 +11,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.loginpagev2); //// BYT EJ.
 
         FirebaseDatabase database = FirebaseDatabase.getInstance("https://meetr-android-default-rtdb.europe-west1.firebasedatabase.app/");
         NewSession newSession = new NewSession(database);
